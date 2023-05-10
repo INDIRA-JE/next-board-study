e34fef568abde68b3f9059aa1bca5ac26ac8ae5a
JE <whddms87@gmail.com>
2023-05-11 03:13:55 +0900

    Next.js에서 MongoDB 연결 & 사용

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import { MongoClient } from "mongodb";
+import { connectDB } from "@/util/database.js";
+
+export default async function Home() {
+  // MongoDB 연결 코드
+  const client = await connectDB;
+  const db = client.db("forum");
+  let result = await db.collection("post").find().toArray();
+  // db에 있는 post collection에 있는 모든 데이터를 가져와서 Array로 변환
+  console.log(result);
+
+  return <main>{result[0].title}</main>;
+}
