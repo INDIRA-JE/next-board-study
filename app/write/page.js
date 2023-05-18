9dcd94ae28dc31c85ec7dd9a84216b40f8da2d1f
JE <whddms87@gmail.com>
2023-05-18 18:33:07 +0900

    글 작성기능 만들기 1 - 304 Not Modified 해결

--- app/write/page.js
+++ app/write/page.js
@@ -2,11 +2,11 @@ export default function Write() {
   return (
     <div>
       <h3>글 작성</h3>
-      <form action="/api/test" method="GET">
-        <button type="submit">GET(200) 버튼</button>
-      </form>
       <form action="/api/test" method="POST">
-        <button type="submit">POST(200) 버튼</button>
+        <button type="submit">POST 버튼</button>
+      </form>
+      <form action="/api/test" method="GET">
+        <button type="submit">GET 버튼</button>
       </form>
     </div>
   );
