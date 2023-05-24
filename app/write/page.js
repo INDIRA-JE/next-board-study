f224bcd8bdb89bc81a0b562e832a6a55a35092b6
JE <whddms87@gmail.com>
2023-05-24 18:12:03 +0900

    글 작성기능 만들기 1 - write page 작성

--- app/write/page.js
+++ app/write/page.js
@@ -1,13 +1,19 @@
 export default function Write() {
   return (
-    <div>
+    <div className="p-20">
       <h3>글 작성</h3>
-      <form action="/api/test" method="POST">
+      <form action="api/post/new" method="POST">
+        <input name="title" placeholder="글 제목 작성" />
+        <input name="content" placeholder="글 내용 작성" />
+        <button type="submit">저장</button>
+      </form>
+
+      {/* <form action="/api/test" method="POST">
         <button type="submit">POST 버튼</button>
       </form>
       <form action="/api/test" method="GET">
         <button type="submit">GET 버튼</button>
-      </form>
+      </form> */}
     </div>
   );
 }
