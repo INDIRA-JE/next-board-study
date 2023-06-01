8593248c614af2b6aed65eb35cd860e3677134d6
JE <whddms87@gmail.com>
2023-06-01 15:06:19 +0900

    수정기능 만들기 1 - 개인코딩 완료

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -6,36 +6,33 @@ export default async function edit(props) {
   let result = await db
     .collection("post")
     .findOne({ _id: new ObjectId(props.params.id) });
-  console.log(result);
-  /* 
-    {
-        _id: new ObjectId("645cee323d5c717e29a895ac"), 
-        title: '두번째글', 
-        content: '두번째 글내용'
-    }
-  */
-
-  //   await db.collection("post").updateOne({수정할 게시물 정보}, {수정할 내용});
-  //   await db.collection("post").updateOne({_id : "게시물 아이디"}, {$set : {title:"제목 수정", content:"내용 수정"} });
-
+  // .findOne() : DB에서 id값을 찾음(props를 통해서 들어오는 id값 경로 적용)
+  console.log("★★★ props | ================\n", props);
+  // props : { params: { id: '645bb2575888a7fef7d31f7d' }, searchParams: {} }
+  console.log("★★★ result | ================\n", result);
+  // result : { _id: new ObjectId("645bb2575888a7fef7d31f7d"), title: '안녕', content: '반가워'}
   return (
     <div>
       <h3>수정 페이지</h3>
       <form action="api/post/new" method="POST">
-        <input name="title" placeholder="글 제목" defaultValue={result.title} />
+        <input
+          name="title"
+          placeholder="제목 작성"
+          defaultValue={result.title}
+        />
         <input
           name="content"
-          placeholder="글 내용"
+          placeholder="제목 작성"
           defaultValue={result.content}
         />
-        <button type="submit">수정</button>
+        <button type="submit">수정 완료</button>
       </form>
     </div>
   );
 }
 
-/**
- * 글제목
- * 글 작성 박스(제목, 내용)
- * 버튼
- */
+/*
+  1. 글마다 수정버튼 → 누르면 수정페이지로 이동
+  2. 수정페이지 만들기 (글 가져와서 채워놔야함) - 완료
+  3. 발행누르면 DB에 있던 글 수정
+*/
