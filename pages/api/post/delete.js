32520f46f72f6de1f929abff2ae2a07409a79c86
JE <whddms87@gmail.com>
2023-07-12 02:29:03 +0900

    삭제기능 만들기 2 (Ajax 추가내용과 에러처리)

--- pages/api/post/delete.js
+++ pages/api/post/delete.js
@@ -4,12 +4,15 @@ import { ObjectId } from "mongodb";
 export default async function handler(요청, 응답) {
   // if (요청.method == "DELETE") { ※ DELETE 요청시 데이터가 없을 때 → POST로 사용
   if (요청.method == "POST") {
-    // console.log("\n>>> 요청.body : ", JSON.parse(요청.body).a);
-    console.log("\n>>> 요청.body(_id) : ", 요청.body); // result[i]._id 값
+    // console.log("\n>>> 요청.body(_id) : ", 요청.body); // {"a":1}
+    // console.log("\n>>> 요청.body(_id) : ", 요청.body.a); // undefined
+    // console.log("\n>>> 요청.body(_id).a : ", JSON.parse(요청.body).a); // 1
+
     const db = (await connectDB).db("forum");
     let result = await db
       .collection("post")
       .deleteOne({ _id: new ObjectId(요청.body) });
+    console.log(result); // { acknowledged: true, deletedCount: 1 }
     응답.status(200).json("삭제완료");
   }
 }
