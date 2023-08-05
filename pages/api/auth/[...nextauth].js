77095884a6b792b6190c3819b909d8522b8f8fca
JE <whddms87@gmail.com>
2023-08-06 01:43:05 +0900

    OAuth(NextAuth) 셋팅 - GithubProvider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: "f8aae1365273ea29e0e4",
+      clientSecret: "c1298b62ff52ff99b982b3635b4cd11606ded8ff",
+    }),
+  ],
+  secret: "qwe123",
+};
+export default NextAuth(authOptions);
