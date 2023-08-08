87d9d4d7ad96db7783a285a9bd694e3d9187d280
JE <whddms87@gmail.com>
2023-08-08 22:56:03 +0900

    console.log(확인) - 주석처리

--- pages/api/post/delete.js
+++ pages/api/post/delete.js
@@ -2,11 +2,11 @@ import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 
 export default async function handler(요청, 응답) {
-  console.log("확인 \n", 요청.body);
+  // console.log("확인 \n", 요청.body);
 
   // if (요청.method == "DELETE") { ※ DELETE 요청시 데이터가 없을 때 → POST로 사용
   if (요청.method == "POST") {
-    console.log("\n>>> 요청.body(_id) : ", 요청.body); // {"a":1}
+    // console.log("\n>>> 요청.body(_id) : ", 요청.body); // {"a":1}
     // console.log("\n>>> 요청.body(_id).a : ", 요청.body.a); // undefined
     // console.log("\n>>> 요청.body(_id).a : ", JSON.parse(요청.body).a); // 1
 
@@ -15,7 +15,7 @@ export default async function handler(요청, 응답) {
       .collection("post")
       .deleteOne({ _id: new ObjectId(요청.body) });
 
-    console.log("???????", result); // { acknowledged: true, deletedCount: 1 }
+    // console.log("???????", result); // { acknowledged: true, deletedCount: 1 }
     응답.status(200).json("삭제완료");
   }
 }
