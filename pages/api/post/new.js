da78f49c90901f29fe90bd857219a0e3f461f084
JE <whddms87@gmail.com>
2023-08-08 22:52:35 +0900

    회원기능 만들기 : OAuth + Session 방식 사용하기 - 완료

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,13 +1,22 @@
 import { connectDB } from "@/util/database";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(request, response) {
   // 1. request 들어오는지 확인
   // console.log("◆ 요청 =========\n", request.method);
   // console.log("◆ 요청 =========\n", request.body);
 
+  // 유저정보 DB에서 가져오기
+  let session = await getServerSession(request, response, authOptions);
+  console.log("\n### WriteSession\n", session);
+  if (session) {
+    request.body.author = session.user.email;
+  }
+  console.log("\n### request.body\n", request.body);
+
   // 4. 반복문 작성
   if (request.method == "POST") {
-    // console.log("● request.method\n", request.method);
     if (request.body.title == "") {
       response.status(500).json("너 왜 제목 작성안해??");
     } else if (request.body.content == "") {
