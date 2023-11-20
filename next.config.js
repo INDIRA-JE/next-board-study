8618db40e0dcbd90f1c25d2efa49bae3dcb581a4
JE <whddms87@gmail.com>
2023-11-20 20:40:23 +0900

    사전준비 작업(버전, config 코드 추가)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // 버전 13까지 적용, 14이상 건드릴 필요없음
+  experimental: {
+    appDir: true,
+    serverActions: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
